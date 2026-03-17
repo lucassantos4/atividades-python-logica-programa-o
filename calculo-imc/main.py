089167ac26dbd1a46ff0f1c365b500d3dc4f89fc
Lucas Rocha Santos <lucas.santos04@souunit.com.br>
2026-03-16 22:25:26 -0300

    calculo-imc

---
+++ calculo-imc/main.py
@@ -0,0 +1,25 @@
+def calcular_imc(peso, altura):
+
+    imc = peso / (altura ** 2)
+    return imc
+
+
+def classificar_imc(imc):
+    if imc < 0:
+        return "IMC inválido. "
+    elif imc < 18.5:
+        return "Abaixo do peso"
+    elif imc < 25:
+        return "Peso normal"
+    elif imc < 30:
+        return "Sobrepeso"
+    else:
+        return "Obesidade"
+
+
+if __name__ == "__main__":
+
+    peso = float(input("Digite seu peso em kg: "))
+    altura = float(input("Digite sua altura em metros: "))
+    imc = calcular_imc(peso, altura)
+    print(f"Seu IMC é: {imc:.2f}, vocé esta {classificar_imc(imc)}.")
